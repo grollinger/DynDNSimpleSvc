b9e6830f957b1fc8008996001b96885478ab5be3
Georg Rollinger <georg.rollinger@web.de>
2013-08-17 15:45:42 +0200

    init (1.0.0.0) - DynDNS Update - As a Service - Event Logging

---
+++ DynDNSimpleSvc/DynDNSimple.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using DNSimple;
+using System.Net.Http;
+using System.Threading;
+
+namespace DynDNSimpleSvc
+{
+    public partial class DynDNSimple : ServiceBase
+    {
+        public const string EventSource = "DynDNSSimpleSvc";
+
+        DNSimpleRestClient _DNSimple;
+        string _Domain, _RecordName;
+        int _RecordID;
+        Timer _Timer;
+
+        public DynDNSimple()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            OnStart();
+        }
+
+        public void OnStart()
+        {
+            var interval = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["updateinterval_minutes"]);
+            var user = System.Configuration.ConfigurationManager.AppSettings["username"];
+            var pass = System.Configuration.ConfigurationManager.AppSettings["password"];
+            _Domain = System.Configuration.ConfigurationManager.AppSettings["domain"];
+            _RecordID = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["recordid"]);
+            _RecordName = System.Configuration.ConfigurationManager.AppSettings["recordname"];
+
+            
+
+            _DNSimple = new DNSimpleRestClient(user, pass);
+
+            _Timer = new Timer(OnTick);
+
+            var intervalMs = interval * 1000 * 60;
+
+            _Timer.Change(1, intervalMs);
+        }
+
+        void OnTick(object state)
+        {
+            Console.WriteLine("Tick");
+            update_dns();            
+        }
+
+        async void update_dns()
+        {
+            try
+            {
+                var ip_json = await new HttpClient().GetStringAsync("http://jsonip.com");
+
+                var rdr = new JsonFx.Json.JsonReader();
+
+                dynamic json = rdr.Read(ip_json);
+                string ip = (string)json.ip;
+
+                var response = _DNSimple.UpdateRecord(_Domain, _RecordID, _RecordName, ip);
+            }
+            catch (Exception ex)
+            {
+                
+
+                EventLog.WriteEntry(EventSource,
+                    string.Format("An Exception occurred:\n {0}", ex.ToString()), 
+                    EventLogEntryType.Warning, 1);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            OnStopped();
+        }
+        public void OnStopped()
+        {
+            _Timer.Dispose();
+        }
+    }
+}
---
+++ DynDNSimpleSvc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynDNSimpleSvc
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+#if(!DEBUG)
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Checking Event Log...");
+                if (!EventLog.SourceExists(DynDNSimple.EventSource))
+                {
+                    Console.WriteLine("Creating EventLog");
+                    EventLog.CreateEventSource(DynDNSimple.EventSource, "Application");
+                }
+                else
+                {
+                    Console.WriteLine("...exists");
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+                { 
+                    new DynDNSimple() 
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+#else
+            var svc = new DynDNSimple();
+            svc.OnStart();
+            Console.WriteLine("Test");
+            Console.ReadLine();
+            svc.OnStopped();
+#endif
+        }
+    }
+}
